9f39e36392b95adca49c607a0b1e1f2f18f91b40
JoshiNirdesh <joshinirdesh10@gmail.com>
2025-12-20 15:05:54 +0545

    random generator

--- JS/script.js
+++ JS/script.js
@@ -54,62 +54,76 @@
 //     alert(`The result is ${eval(`${a} ${c} ${b}`)}`);
 // }
 
-let numbers = [1,2,3,4,5,6];
+// let numbers = [1,2,3,4,5,6];
 
-console.log(numbers.toString());
-console.log(numbers.join("-"));
+// console.log(numbers.toString());
+// console.log(numbers.join("-"));
 
-numbers.pop();
-console.log(numbers);
+// numbers.pop();
+// console.log(numbers);
 
-numbers.push(12);
-console.log(numbers);
+// numbers.push(12);
+// console.log(numbers);
 
-numbers.shift();
-console.log(numbers);
+// numbers.shift();
+// console.log(numbers);
 
-numbers.unshift(100);
-console.log(numbers)
-
-
-console.log(numbers.length);
-
-let a = [1,2,3]
-let b = [4,5,6]
-let c = [7,8,9];
-
-let d = a.concat(b,c);
-
-console.log(d);
-
-let num = [1,4,5,67,8,4];
-
-num.splice(1,2);
-console.log(num);
-
-num.splice(1,3,6,7);
-console.log(num)
-
-let numb = [1,2,3,4,5,6,7,8];
-
-numb.forEach((value,index)=>{
-   console.log(value);
-})
-
-numb.map((e)=>{
-   console.log(e+2);
-})
-
-let even =  numb.filter(numb => numb%2==0);
-console.log(even);
-
-let users =  [
-   {name: "Ram", age : 6},
-   {name : "Shyam", age : 17},
-   {name: "hari", age:12}
-]
-
-let newUser = users.filter(u=>u.age>10 );
-console.log(newUser);
+// numbers.unshift(100);
+// console.log(numbers)
 
 
+// console.log(numbers.length);
+
+// let a = [1,2,3]
+// let b = [4,5,6]
+// let c = [7,8,9];
+
+// let d = a.concat(b,c);
+
+// console.log(d);
+
+// let num = [1,4,5,67,8,4];
+
+// num.splice(1,2);
+// console.log(num);
+
+// num.splice(1,3,6,7);
+// console.log(num)
+
+// let numb = [1,2,3,4,5,6,7,8];
+
+// numb.forEach((value,index)=>{
+//    console.log(value);
+// })
+
+// numb.map((e)=>{
+//    console.log(e+2);
+// })
+
+// let even =  numb.filter(numb => numb%2==0);
+// console.log(even);
+
+// let users =  [
+//    {name: "Ram", age : 6},
+//    {name : "Shyam", age : 17},
+//    {name: "hari", age:12}
+// ]
+
+// let newUser = users.filter(u=>u.age>10 );
+// console.log(newUser);
+
+//First 
+// crazy 
+// Amazing
+// Fire
+// //Second 
+// Engine
+// Foods
+// Graments
+// //Thirds
+// Bros 
+// Limited 
+// Hub 
+
+
+//  
