16badb4100f584ce8c824c33665dd6e3349909fb
JoshiNirdesh <joshinirdesh10@gmail.com>
2025-12-25 08:51:31 +0545

    404 page and path improvement

--- express/index.js
+++ express/index.js
@@ -1,15 +1,19 @@
 import express from "express";
-import path from "path"
+import path from "path";
+
 const absPath = path.resolve("view")
 const app = express();
+
 app.get("/",(req,res)=>{
     res.sendFile(absPath+"/home.html");
-})
-app.get("/login",(req,res)=>{
-    res.sendFile(absPath+"/login.html");
 })
 app.get("/about",(req,res)=>{
     res.sendFile(absPath+"/about.html");
 })
-
-app.listen(3000);
+app.get("/login",(req,res)=>{
+    res.sendFile(absPath+"/login.html");
+})
+app.use((req,res)=>{
+    res.sendFile(absPath+"/404.html")
+})
+app.listen(4000);
