1fba502b25f04a55e97ea14afc40497ed859d5bb
JoshiNirdesh <joshinirdesh10@gmail.com>
2025-12-27 11:00:18 +0545

    PUT API

--- express/index.js
+++ express/index.js
@@ -1,35 +1,36 @@
-import express from 'express'
-import mongoose from "mongoose"
-import studentModel from './model/studentModel.js';
+import express from "express";
+import mongoose from "mongoose";
+import studentModel from "./model/studentModel.js";
 
 const app = express();
-app.use(express.json());
+app.use(express.json())
 
 await mongoose.connect("mongodb://localhost:27017/school").then(()=>{
-    console.log("Database Connected");
+    console.log("DataBase Connected ")
 })
 
 app.get("/",async(req,res)=>{
     const studentData = await studentModel.find();
     res.send(studentData);
 })
-app.post("/save",(req,res)=>{
-    const {name,age,email}=req.body;
-    if(!req.body || !name || !age ||!email){
-        res.send({ 
-        message:"data not Stroed",
-        success :false,
-        storedData : null})
 
-        return false
-    }
-    
-    
-    const studentData = studentModel.create(req.body);
+app.post("/save",async(req,res)=>{
+
+    const studentData = await studentModel.create(req.body);
     res.send({
-        message:"data Stroed",
-        success :true,
-        storedData : req.body
+        message: "Data stored",
+        success:true,
+        stored:studentData
     })
+})
+app.put("/update/:id",async (req,res)=>{
+    const id =req.params.id;
+    const studentData = await studentModel.findByIdAndUpdate(id,{...req.body});
+    res.send({
+        message:"Data updated",
+        success:true,
+        Stored:studentData,
+    })
+
 })
 app.listen(4000);
