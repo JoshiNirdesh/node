3bd9d2fab3ec9fc7e5cb91461988ff91f4ecb5b3
JoshiNirdesh <joshinirdesh10@gmail.com>
2026-01-13 13:19:52 +0545

    delete user api

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,6 @@
 const userModel = require("../models/userModel")
+const bcrypt = require("bcrypt");
+const JWT = require("jsonwebtoken")
 
 const getUserController = async (req, res) => {
     try {
@@ -53,4 +55,56 @@ const udpdateUserController = async (req, res) => {
     }
 
 }
-module.exports = { getUserController, udpdateUserController }
+const updatePasswordController = async (req, res) => {
+    try {
+        const user = await userModel.findById({ _id: req.userId });
+
+        if (!user) {
+            return res.status(500).send({
+                success: false,
+                message: "User not found"
+            })
+        }
+        const { oldPassword, newPassword } = req.body
+
+        const isMatch = await bcrypt.compare(oldPassword, user.password);
+        if (!isMatch) {
+            return res.status(500).send({
+                success: false,
+                message: "Old password doesnot match"
+            })
+        }
+        const salt = await bcrypt.genSalt(10);
+        const hashPassword = await bcrypt.hash(newPassword, salt);
+        user.password = hashPassword
+        await user.save();
+        res.send(200).send({
+            success: true,
+            message: "Password reset successfully"
+        })
+    } catch (error) {
+        res.send(500).send({
+            success: false,
+            message: "Update Password API error",
+            error: error.message
+        })
+    }
+}
+const deleteUserController = async (req, res) => {
+    try {
+        await userModel.findByIdAndDelete({ _id: req.params.id })
+        res.status(200).send({
+            success: true,
+            message: "Deleted User Successfully",
+        })
+
+    } catch (error) {
+        res.status(500).send({
+            success: false,
+            message: "Delete User Api error",
+            error: error.message
+        })
+
+    }
+}
+module.exports = { getUserController, udpdateUserController, updatePasswordController, deleteUserController }
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,13 @@
 const express = require("express");
-const { getUserController, udpdateUserController } = require("../controllers/userController");
+const { getUserController, udpdateUserController, updatePasswordController, deleteUserController } = require("../controllers/userController");
 const authMiddleware = require("../middleware/authMiddleware");
 
 
 const router = express.Router();
 router.get("/getuser", authMiddleware, getUserController);
-router.get("/updateuser", authMiddleware, udpdateUserController)
+router.post("/updateuser", authMiddleware, udpdateUserController)
+router.post("/updatepassword", authMiddleware, updatePasswordController)
+router.delete("/deleteuser/:id", authMiddleware, deleteUserController)
+
+
 module.exports = router
