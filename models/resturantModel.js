841cf6ee4a01b1e300c77ea85c13e55eb32e8aab
JoshiNirdesh <joshinirdesh10@gmail.com>
2026-01-17 07:39:07 +0545

    resturant Model

--- models/resturantModel.js
+++ models/resturantModel.js
@@ -1,13 +1,57 @@
 const mongoose = require("mongoose");
 
 const resturantSchema = mongoose.Schema({
-    title:{
-        type:String,
-        required:[true,"Resturant Title is required"]
+    title: {
+        type: String,
+        required: [true, "Resturant Title is required"]
     },
-    imageURL
+    imageUrl: {
+        type: String,
+    },
+    foods: {
+        type: Array
+    },
+    time: {
+        type: String
+    },
+    pickup: {
+        type: Boolean,
+        default: true,
+    },
+    delivery: {
+        type: Boolean,
+        default: true
+    },
+    isOpen: {
+        type: Boolean,
+        default: true,
+    },
+    logoUrl: {
+        type: String,
+    },
+    rating: {
+        type: Number,
+        default: 1,
+        min: 1,
+        max: 5
+    },
+    ratingCount: {
+        type: String
+    },
+    code: {
+        type: String
+    },
+    coords: {
+        id: { type: String },
+        latitude: { type: Number },
+        latitudeDelta: { type: Number },
+        longitude: { type: Number },
+        longitidueDelta: { type: Number },
+        address: { type: String },
+        title: { type: String }
+    }
 
-});
+},{timestamps:true});
 
 
 const resturantModel = mongoose.model("resturant", resturantSchema)
